1e1989795a3de5257c6006ec6d3457786dff9463
Yeahia Md. Arif <yeahia.arif@gmail.com>
2018-10-27 16:48:10 +0600

    worked google Sign in

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterchat/main.dart';
+import 'package:flutterchat/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
